d72a766f1d4f5704b132ffa3c1438e359556f1a4
GeorgeC <collingridgegeorge@gmail.com>
2021-04-22 15:40:47 +0100

    basic dice game made for reference

---
+++ Pigs-Probabilities.md
@@ -0,0 +1,10 @@
+- Pig out -
+- Sider -
+- Trotter -
+- Double Trotter -
+- Razorback -
+- Double Razorback -
+- Snouter -
+- Double Snouter -
+- Leaning Jowler -
+- Double Leaning Jowler -
--- back-end/Program.cs
+++ back-end/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Threading;
 
-namespace back_end
+namespace backend
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            // int number = rand.Next(1, 5);
+            // System.Console.WriteLine(number);
+
+            // int[] numbers = new int[10];
+            // for (int i = 0; i < numbers.Length; i++)
+            // {
+            //     numbers[i] = rand.Next(1, 50);
+            // }
+
+            // foreach (var n in numbers)
+            // {
+            //     System.Console.WriteLine(n + " ");
+            // }
+
+            int dice1 = rand.Next(1, 7);
+            int dice2 = rand.Next(1, 7);
+
+            System.Console.WriteLine("Dice1: " + dice1);
+            System.Console.WriteLine("Dice2: " + dice2);
+
+            if (dice1 > dice2)
+            {
+                System.Console.WriteLine("dice1 wins");
+
+            }
+            else if (dice2 > dice1)
+            {
+                System.Console.WriteLine("dice2 wins");
+
+            }
+            else
+            {
+                System.Console.WriteLine("draw");
+            }
+
+            Console.ReadLine();
         }
     }
 }
