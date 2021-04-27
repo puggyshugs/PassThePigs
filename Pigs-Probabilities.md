c7e391de30457c328fa1dfe1987910026818e7d2
GeorgeC <collingridgegeorge@gmail.com>
2021-04-27 13:02:58 +0100

    Methods added to class

--- Pigs-Probabilities.md
+++ Pigs-Probabilities.md
@@ -1,15 +1,15 @@
 - Pig out -
 - Sider 1 point -
 - Trotter 5 points-
-- Double Trotter 20 points-
-- Razorback 5 points-
-- Double Razorback 20 points-
-- Snouter 10 points-
-- Double Snouter 40 points -
-- Leaning Jowler 15 points-
-- Double Leaning Jowler 60 points-
+- Double Trotter, 20 points-
+- Razorback, 5 points-
+- Double Razorback, 20 points-
+- Snouter, 10 points-
+- Double Snouter, 40 points -
+- Leaning Jowler, 15 points-
+- Double Leaning Jowler, 60 points-
 - Mixed Combo (addition of points, e.g. razor back + trotter = 10)
-- Making Bacon (Lose all your points) -
+- Making Bacon, (Lose all your points) -
 
 <br/>
 
--- back-end/PassPigs.cs
+++ back-end/PassPigs.cs
@@ -6,6 +6,37 @@ namespace PassThePigsGames
 {
     class PassPigs
     {
+        private int Score;
 
+        public PassPigs()
+        {
+            //initialise anything we need later on:
+        }
+
+        public void start()
+        {
+            //Method that starts the game.
+
+        }
+
+        private void PlayRound()
+        {
+            // Method that runs one round of rolling and guessing
+        }
+
+        private void Win()
+        {
+            // Method that increments the score and lets the player know they won
+        }
+
+        private void Lose()
+        {
+            // Method that lets the player know they lost
+        }
+
+        private void AskToPlayAgain()
+        {
+            // Ask player if they want to play another round
+        }
     }
 }
