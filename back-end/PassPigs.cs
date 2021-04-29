49b0ee0ab0a2abad243a6fc6b1f0bccb95c117ab
GeorgeC <collingridgegeorge@gmail.com>
2021-04-29 14:33:59 +0100

    Basic round 1 added

--- back-end/PassPigs.cs
+++ back-end/PassPigs.cs
@@ -8,14 +8,16 @@ namespace PassThePigsGames
 {
     class PassPigs
     {
-        private string GameName;
         private int Score;
+        private string GameName;
+        private Random RandomGenerator;
 
         public PassPigs()
         {
             //initialise anything we need later on:
             Score = 0;
             GameName = "Pass The Pigs";
+            RandomGenerator = new Random();
         }
 
         public void Start()
@@ -31,23 +33,84 @@ namespace PassThePigsGames
             System.Console.WriteLine("\t> But be warned! Rolling a Pig Out will result in 0 points for that roll.");
             System.Console.WriteLine("\nHave fun! Ready to play? (yes/no)");
 
-            string playResponse = ReadLine();
+            string playResponse = ReadLine().Trim().ToLower();
             if (playResponse == "yes")
             {
-                System.Console.WriteLine("The Pigs will sing to your honour!");
+                System.Console.WriteLine("\n  === The Pigs will sing to your honour! ===  ");
+                PlayRound();
+
             }
             else
             {
-                System.Console.WriteLine("The Pigs are displeased. They will not forget this desertion.");
+                System.Console.WriteLine("\nThe Pigs are displeased. They will not forget this desertion.");
+
             }
-
-            System.Console.WriteLine("Press any key to exit");
-            ReadKey();
+            System.Console.WriteLine("\nPress any key to exit");
         }
-
         private void PlayRound()
         {
             // Method that runs one round of rolling and guessing
+            Clear();
+            System.Console.WriteLine("Pass these Pigs or bank your points? (pass/bank)");
+            string response = ReadLine().Trim().ToLower();
+            System.Console.WriteLine($"You want to {response}.");
+
+            int PigPass = RandomGenerator.Next(1, 12);
+            if (PigPass == 1)
+            {
+                System.Console.WriteLine("You are a stinky pig and you have pigged out! Player 2's go!");
+
+            }
+            else if (PigPass == 2)
+            {
+                System.Console.WriteLine("Sider, 1 point");
+
+            }
+            else if (PigPass == 3)
+            {
+                System.Console.WriteLine("Trotter, 5 points!");
+
+            }
+            else if (PigPass == 4)
+            {
+                System.Console.WriteLine("Double Trotter, 20 points");
+
+            }
+            else if (PigPass == 5)
+            {
+                System.Console.WriteLine("Razorback, 5 points");
+
+            }
+            else if (PigPass == 6)
+            {
+                System.Console.WriteLine("Double Razorback, 20 points");
+
+            }
+            else if (PigPass == 7)
+            {
+                System.Console.WriteLine("Snouter, 10 points");
+
+            }
+            else if (PigPass == 8)
+            {
+                System.Console.WriteLine("Double Snouter, 40 points");
+
+            }
+            else if (PigPass == 9)
+            {
+                System.Console.WriteLine("Leaning Jowler, 15 points");
+
+            }
+            else if (PigPass == 10)
+            {
+                System.Console.WriteLine("Double Leaning Jowler, 60 points");
+
+            }
+            else if (PigPass == 11)
+            {
+                System.Console.WriteLine("Making Bacon - You've lost all of you points");
+
+            }
         }
 
         private void Win()
