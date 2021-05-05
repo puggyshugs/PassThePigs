1f0b0f07277f368ab91d3d99ac9279f5443a91d9
GeorgeC <collingridgegeorge@gmail.com>
2021-05-05 19:14:04 +0100

    update win function

--- back-end/PassThePigs.cs
+++ back-end/PassThePigs.cs
@@ -58,12 +58,19 @@ namespace PassThePigsGame.PassThePigsClass
                 PlayRound();
 
             }
-            else
+            else if (playResponse == "no")
             {
                 Clear();
                 System.Console.WriteLine("\nThe Pigs are displeased. They will not forget this desertion.");
 
             }
+            else
+            {
+                System.Console.WriteLine("Invalid response, press any key to continue.");
+                ReadKey();
+                Start();
+            }
+
             System.Console.WriteLine("\nPress any key to exit");
         }
         private void PlayRound()
@@ -82,66 +89,56 @@ namespace PassThePigsGame.PassThePigsClass
                 {
                     case PigThrows.PigOut:
                         Score = 0;
-                        System.Console.WriteLine("PIG OUT!!!");
-                        System.Console.WriteLine("The Pigs have spoken, you earn nothing for this pitiful attempt...");
+                        System.Console.WriteLine("PIG OUT!!!\nThe Pigs have spoken, you earn nothing for this pitiful attempt...");
                         break;
                     case PigThrows.Sider:
                         Score = Score + 1;
-                        System.Console.WriteLine("Sider, 1 point");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Sider, 1 point\nYour score is {Score}");
                         break;
                     case PigThrows.Trotter:
                         Score = Score + 5;
-                        System.Console.WriteLine("Trotter, 5 points!");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Trotter, 5 points!\nYour score is {Score}");
                         break;
                     case PigThrows.DoubleTrotter:
                         Score = Score + 20;
-                        System.Console.WriteLine("Double Trotter, 20 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Double Trotter, 20 points\nYour score is {Score}");
                         break;
                     case PigThrows.Razorback:
                         Score = Score + 5;
-                        System.Console.WriteLine("Razorback, 5 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Razorback, 5 points\nYour score is {Score}");
                         break;
                     case PigThrows.DoubleRazorback:
                         Score = Score + 20;
-                        System.Console.WriteLine("Double Razorback, 20 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Double Razorback, 20 points\nYour score is {Score}");
                         break;
                     case PigThrows.Snouter:
                         Score = Score + 10;
-                        System.Console.WriteLine("Snouter, 10 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Snouter, 10 points\nYour score is {Score}");
                         break;
                     case PigThrows.DoubleSnouter:
                         Score = Score + 40;
-                        System.Console.WriteLine("Double Snouter, 40 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Double Snouter, 40 points\nYour score is {Score}");
                         break;
                     case PigThrows.LeaningJowler:
                         Score = Score + 15;
-                        System.Console.WriteLine("Leaning Jowler, 15 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Leaning Jowler, 15 points\nYour score is {Score}");
                         break;
                     case PigThrows.DoubleLeaningJowler:
                         Score = Score + 60;
-                        System.Console.WriteLine("Double Leaning Jowler, 60 points");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"Double Leaning Jowler, 60 points\nYour score is {Score}");
                         break;
                     case PigThrows.MakingBacon:
                         Score = 0;
-                        System.Console.WriteLine("You've been caught making bacon");
-                        System.Console.WriteLine("\nThis is not the time OR place for such fornication.");
-                        System.Console.WriteLine("\nThe Pigs are disgusted.");
-                        System.Console.WriteLine($"Your score is {Score}");
+                        System.Console.WriteLine($"You've been caught making bacon\n\nThis is not the time OR place for such fornication.\n\nThe Pigs are disgusted.\nYou lose.\n\nPress any key to continue");
+                        ReadKey();
+                        Lose();
                         break;
                 }
             }
+
             else
             {
-                System.Console.WriteLine("Invalid input.\nPress any button to continue.");
+                System.Console.WriteLine("Invalid input.\nPress any button to continue.\n");
                 ReadKey();
                 PlayRound();
             }
@@ -152,35 +149,33 @@ namespace PassThePigsGame.PassThePigsClass
             }
             else
             {
-                // System.Console.WriteLine("\n\t* Press any key to continue *  ");
-                // ReadKey();
                 PlayAgainOrBank();
             }
 
-            PlayAgainOrBank();
         }
 
         private void Win()
         {
             // Method that increments the score and lets the player know they won
-            System.Console.WriteLine("\n\t=== The Pigs favour you. ===");
-            System.Console.WriteLine("\n\t=== Congratulations, Pig. ===");
-            System.Console.WriteLine("\nPress any key to continue.");
+            Clear();
+            System.Console.WriteLine("\n\t=== The Pigs favour you. ===\n\n\t=== Congratulations, Pig. ===\n\n\tPress any key to continue.\n");
 
         }
 
         private void Lose()
         {
             // Method that lets the player know they lost
+            Clear();
+            System.Console.WriteLine("You have dishonoured the pigs.\nTonight, it would be wise to sleep with one eye open...");
         }
 
         private void PlayAgainOrBank()
         {
             // Ask player if they want to play another round
-            Write("Pass these pigs again or bank your points? (pass/bank)");
+            Write("-PASS- these pigs again or -BANK- your points? (p/b)");
             string playAgainResponse = ReadLine().Trim().ToLower();
 
-            if (playAgainResponse == "pass")
+            if (playAgainResponse == "p")
             {
                 PlayRound();
             }
