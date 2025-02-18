646a0f6c209c7c969103aff6bdc82900f628bd9b
George Collingridge <georgecollingridge@gmail.com>
2025-02-18 12:17:04 +0530

    create the domain layer and move the models there

--- PassThePigs.GameLogic/Helpers/PlayerLogicHelper.cs
+++ PassThePigs.GameLogic/Helpers/PlayerLogicHelper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using PassThePigs.Domain;
 using PassThePigs.Services.Cache.CacheModels;
 
 namespace PassThePigs.GameLogic.Helpers
