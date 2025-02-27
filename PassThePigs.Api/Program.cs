4edbe1825d7720c2bc31a2252a0c4e771ce523ca
George Collingridge <georgecollingridge@gmail.com>
2025-02-27 14:20:55 +0530

    create startup file

--- PassThePigs.Api/Program.cs
+++ PassThePigs.Api/Program.cs
@@ -1,55 +1,12 @@
-using PassThePigs.Hub.Hubs;
-using PassThePigs.Services.Cache.Interfaces;
-using PassThePigs.Services.Interfaces;
-using PassThePigs.Services.Services;
+using PassThePigs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-builder.Services.AddSignalR();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "PassThePigs.Api", Version = "v1" });
-    c.AddSignalRSwaggerGen();
-});
-builder.Services.AddMemoryCache();
-builder.Services.AddSingleton<IGameMemoryCache, GameMemoryCache>();
-builder.Services.AddTransient<IGameCacheService, GameCacheService>();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials()
-              .WithOrigins("http://localhost:3000"); // React frontend URL
-    });
-});
+var startup = new Startup();
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. 
-app.UseCors("CorsPolicy");
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
-    });
-}
-
-app.MapHub<GameHub>("/GameHub");
-Console.WriteLine("SignalR Hub registered at /GameHub");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ PassThePigs.Api/Startup.cs
@@ -0,0 +1,66 @@
+using PassThePigs.Hub.Hubs;
+using PassThePigs.Services.Cache.Interfaces;
+using PassThePigs.Services.Interfaces;
+using PassThePigs.Services.Services;
+using Microsoft.OpenApi.Models;
+using PassThePigs.GameLogic.Services;
+using PassThePigs.Services.Helpers;
+
+namespace PassThePigs
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSignalR();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PassThePigs.Api", Version = "v1" });
+                c.AddSignalRSwaggerGen();
+            });
+            services.AddMemoryCache();
+            services.AddSingleton<IGameMemoryCache, GameMemoryCache>();
+            services.AddTransient<IGameCacheService, GameCacheService>();
+            services.AddTransient<IGameLogicService, GameLogicService>();
+            services.AddTransient<PlayerLogicHelper, PlayerLogicHelper>();
+            services.AddTransient<PigThrowLogicHelper, PigThrowLogicHelper>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials()
+                          .WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5233");
+                    //.SetIsOriginAllowed(origin => true); // React frontend URL, change for variable once deployed
+                });
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                    c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+                });
+            }
+
+            app.MapHub<GameHub>("/GameHub");
+            app.UseCors("CorsPolicy");
+
+            Console.WriteLine("SignalR Hub registered at /GameHub");
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+    }
+}
